34764158eb163f95ef1ea5b81b6c9b3c9dcf7e30
AjaykumarPachigulla <ajaypachigullakumar@gmail.com>
2026-02-17 10:41:33 +0530

    Update backend.tf

--- ecr-terraform/backend.tf
+++ ecr-terraform/backend.tf
@@ -7,12 +7,13 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "ajaypachigulla2"
+    bucket = "ajaypach2"
     key    = "ecr/terraform.tfstate"
     region = "us-east-1"
   }
 
   required_version = ">= 1.6.3"
 }
+
 
 
